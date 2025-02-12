39d552c28d1277e0f7d40834228f58ead34eb7fb
gsand1 <gsand@stu.socsd.org>
2025-02-12 10:01:41 -0500

    gg

---
+++ Comments.java
@@ -0,0 +1,15 @@
+/************************************
+*Project: Demo for Comments
+*Programmer: Gabe Sand
+*Date: Febuary 2025
+*Program Name: Comments.java
+*************************************/
+import java.io.*;
+public class Comments
+{
+    public static void main (String[ ] args)
+    {
+    System.out.println("Hi"); //message to user
+    System.out.println("LabOneA"); //assignment
+    }
+}
---
+++ ThreeExamples.java
@@ -0,0 +1,7 @@
+public class ThreeExamples {
+    public static void main (String[]args)
+    {
+    System.out.println("\n\tDog Show Winners\n\nPlace: \t1\t2\t3\t4\t5\t6\n\tDog#\t76\t32\t18\t47\t11\t29");
+    }
+
+}
