ce1459e32c3220668e377f77d9c77946d1b4130d
gsand1 <gsand@stu.socsd.org>
2025-02-13 09:48:16 -0500

    Update ThreeExamples.java

--- ThreeExamples.java
+++ ThreeExamples.java
@@ -1,7 +1,14 @@
 public class ThreeExamples {
     public static void main (String[]args)
+    // {
+    // System.out.println("\n\tDog Show Winners\n\nPlace: \t1\t2\t3\t4\t5\t6\nDog#\t76\t32\t18\t47\t11\t29");
+    // }
+    // {
+    // System.out.println("Q\"yz89\"Ja4\"tp");
+    // }
     {
-    System.out.println("\n\tDog Show Winners\n\nPlace: \t1\t2\t3\t4\t5\t6\n\tDog#\t76\t32\t18\t47\t11\t29");
+    System.out.println("Anderson\t3452\nBoyton\t\t5344\nCarrington\t3677\nHouston\t\t5988\nMcDougal\t5233\nRobertson\t3623\nStryker\t\t5112");
     }
+
 
 }
